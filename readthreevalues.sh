896f08f911ea66584396308767f9a93d1ca06111
srikanth1009 <srikanthreddyyaddala8.com>
2021-09-02 13:22:50 +0530

    read three values

--- readthreevalues.sh
+++ readthreevalues.sh
@@ -1,27 +1,3 @@
 #!/bin/bash -x
 read a b c
-if [ $a -eq $b -a $a -eq $c ]; then
-    echo "All the three numbers are equal"
-elif [[ $a -eq $b || $b -eq $c || $c -eq $a ]]; then
-    echo "I cannot figure out which number is largest"
-else
-echo `expr $a + $b \* $c`
-echo `expr $a % $b + $c`
-echo `expr $c + $a / $b`
-echo `expr $a \* $b + $c`
-    if [ $a -gt $b -a $a -gt $c ]; then
-        echo "$a is biggest number"
-    elif [ $b -gt $a -a $b -gt $c ]; then
-        echo "$b is biggest number"
-    elif [ $c -gt $a -a $c -gt $b ]; then
-        echo "$c is biggest number"
-  elif [ $a -lt $b -a $a -lt $c ]; then
-        echo "$a is smallest number"
-    elif [ $b -lt $a -a $b -lt $c ]; then
-        echo "$b is smallest number"
-    elif [ $c -lt $a -a $c -lt $b ]; then
-        echo "$c is smallest number"
-
-    fi
-fi
-
+echo "the three values are $a $b $c"
